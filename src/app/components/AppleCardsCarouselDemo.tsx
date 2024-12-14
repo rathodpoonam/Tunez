ae8dd30518e9732d0db4623a67efcd209cf44d81
poonam <rathodpoonam334@gmail.comgit config --global user.email rathodpoonam334@gmail.com>
2024-12-14 13:30:14 +0530

    my second commit

--- src/app/components/AppleCardsCarouselDemo.tsx
+++ src/app/components/AppleCardsCarouselDemo.tsx
@@ -2,7 +2,6 @@
 import Image from "next/image";
 import React from "react";
 import { Carousel, Card } from "./ui/apple-cards-carousel";
-// import { useRouter } from "next/router";
 
 
 export function AppleCardsCarouselDemo() {
